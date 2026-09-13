0eda7c597582e4c8729af48df9e9efd2fd1a1384
c-jay69 <simonjc.mex@gmail.com>
2026-09-13 08:48:41 -0600

    fix: Update assetManifest to reference existing logo/font files
    - Replace deleted logo references (mainlogoone.png, mainlogo.png, logo1-13.png) with actual 6 RS logos
    - Remove deleted marcieimage references from avatar frames
    - Fix font file names to match actual files in assets/fonts/
    - Pin async-storage to exact version 1.23.1 to match Expo SDK 52

--- app/src/constants/assetManifest.ts
+++ app/src/constants/assetManifest.ts
@@ -1,53 +1,21 @@
 import { Platform } from 'react-native';
 
 export const CRITICAL_LOGOS: number[] = [
-  require('../../assets/logo/mainlogoone.png'),
+  require('../../assets/logo/RSICONNB.png'),
 ];
 
 export const LAZY_LOGOS: number[] = [
-  require('../../assets/logo/mainlogo.png'),
-  require('../../assets/logo/logo1.png'),
-  require('../../assets/logo/logo2.png'),
-  require('../../assets/logo/logo3.png'),
-  require('../../assets/logo/logo4.png'),
-  require('../../assets/logo/logo7.png'),
-  require('../../assets/logo/logo8.png'),
-  require('../../assets/logo/logo9.png'),
-  require('../../assets/logo/logo10.png'),
-  require('../../assets/logo/logo11.png'),
-  require('../../assets/logo/logo12.png'),
-  require('../../assets/logo/logo13.png'),
+  require('../../assets/logo/RSTRANSPARENTICONNB.png'),
+  require('../../assets/logo/RSBLACKNBSQUARE.png'),
+  require('../../assets/logo/RSWHITENBSQUARE.png'),
+  require('../../assets/logo/RSBLACKNBBANNER.png'),
+  require('../../assets/logo/RSWHITENBBANNER.png'),
 ];
 
 export const LOGO_IMAGES: number[] = [...CRITICAL_LOGOS, ...LAZY_LOGOS];
 
-export const CRITICAL_AVATAR_FRAMES: number[] = [
-  require('../../assets/marcieimages/marcieimage1.png'),
-];
-
-export const LAZY_AVATAR_FRAMES: number[] = [
-  require('../../assets/marcieimages/marcieimage2.png'),
-  require('../../assets/marcieimages/marcieimage3.png'),
-  require('../../assets/marcieimages/marcieimage4.png'),
-  require('../../assets/marcieimages/marcieimage5.png'),
-  require('../../assets/marcieimages/marcieimage6.png'),
-  require('../../assets/marcieimages/marcieimage7.png'),
-  require('../../assets/marcieimages/marcieimage8.png'),
-  require('../../assets/marcieimages/marcieimage9.png'),
-  require('../../assets/marcieimages/marcieimage10.png'),
-  require('../../assets/marcieimages/marcieimage11.png'),
-  require('../../assets/marcieimages/marcieimage12.png'),
-  require('../../assets/marcieimages/marcieimage13.png'),
-  require('../../assets/marcieimages/marcieimage14.png'),
-  require('../../assets/marcieimages/marcieimage15.png'),
-  require('../../assets/marcieimages/marcieimage16.png'),
-  require('../../assets/marcieimages/marcieimage17.png'),
-  require('../../assets/marcieimages/marcieimage18.png'),
-  require('../../assets/marcieimages/marcieimage19.png'),
-  require('../../assets/marcieimages/marcieimage20.png'),
-];
-
-export const AVATAR_FRAMES: number[] = [...CRITICAL_AVATAR_FRAMES, ...LAZY_AVATAR_FRAMES];
+// Marcie avatar frames removed - using video assets instead
+export const AVATAR_FRAMES: number[] = [];
 
 export const FONT_SOURCES: Record<string, number> = {
   'BarbieDream-Regular': require('../../assets/fonts/barbie.ttf'),
